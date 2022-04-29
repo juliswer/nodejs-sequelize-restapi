3d04fc41c39a9f5e4f9e22f8cce271d6368d82ba
juliswer <julianswer36@gmail.com>
2022-04-29 13:25:50 -0300

    routes

--- src/index.js
+++ src/index.js
@@ -1,14 +1,12 @@
 import app from "./app.js";
 import { sequelize } from "./database/database.js";
 import { PORT } from "./config.js";
-import "./models/Project.js";
-import "./models/Task.js";
 
 // ? Starting the app
 
 async function main() {
   try {
-    await sequelize.sync({ force: true });
+    await sequelize.sync({ force: false });
     app.listen(PORT, () => {
       console.log(`Server is running on port ${PORT}`);
     });
---
+++ src/routes/projects.routes.js
@@ -0,0 +1,15 @@
+import { Router } from "express";
+
+const router = Router();
+
+router.get("/projects", () => {});
+
+router.post("/projects")
+
+router.put("/projects/:id", () => {});
+
+router.delete("/projects/:id", () => {});
+
+router.get("/projects/:id", () => {});
+
+export default router;
