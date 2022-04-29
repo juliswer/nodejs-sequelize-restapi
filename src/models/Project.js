2d5c95004ab556123c67c4396c282edfdd168c43
juliswer <julianswer36@gmail.com>
2022-04-28 21:10:24 -0300

    setting tables

---
+++ src/models/Project.js
@@ -0,0 +1,19 @@
+import { DataTypes } from "sequelize";
+import { sequelize } from "../database/database.js";
+
+sequelize.define("projects", {
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true
+  },
+  name: {
+    type: DataTypes.STRING,
+  },
+  priority: {
+    type: DataTypes.INTEGER,
+  },
+  description: {
+    type: DataTypes.STRING,
+  },
+});
